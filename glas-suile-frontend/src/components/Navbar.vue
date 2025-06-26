e9024ebd0cf56d311428110b9fdc7ee1f85f96e3
TrevorBrowning <trevryanbrowning@gmail.com>
2025-06-26 11:36:54 -0400

    Feat: Add Navbar

---
+++ glas-suile-frontend/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <div v-if="!authStore.user">
+      <RouterLink to="/login">Login</RouterLink>
+      <RouterLink to="/register">Register</RouterLink>
+    </div>
+    <div v-else>
+      <RouterLink to="/dashboard">Dashboard</RouterLink>
+      <button @click="authStore.clearAuthData()">Sign Out</button>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
+import { RouterLink } from 'vue-router'
+const authStore = useAuthStore()
+</script>
